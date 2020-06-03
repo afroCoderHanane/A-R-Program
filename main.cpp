b3662d3c3c9711480e7bd02f5783b529222f2c11
hananeDev84 <gbadhanane@gmail.com>
2020-06-02 21:14:57 -0400

    files added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 //
 // Created by hanane on 6/2/20.
 //
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
 
